b494fb141e77720b85f3d21e064211fa25e93c13
Marina Braytseva <losewa.marishka@gmail.com>
2024-09-30 12:58:34 +0300

    Финальный вариант

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header>
             <h1>Contact List on JS</h1>
-            <a href="https://github.com/Mari128/task2-contactList">GitHub Repository link</a>
+            <a href="https://github.com/Mari128/contactlist-ts-react-redux">GitHub Repository link</a>
         </header>
     )
 }
